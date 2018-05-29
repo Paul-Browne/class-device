957efea4cdcca7ce4f77223267bd93d483808c5b
paulbrowne <Paulmatthewbrowne@gmail.com>
2018-05-29 11:01:55 +0300

    Update class-mobile.js

--- class-mobile.js
+++ class-mobile.js
@@ -1,21 +1,27 @@
 (function() {
 
     var dims = {
-        "laptop": 1520,				// 95em
-        "tablet": 1280,				// 80em
-        "tablet-portrait": 1000,	// 62.5em
-        "mobile": 720,				// 45em
-        "mobile-portrait": 440		// 27.5em
+        "laptop": 1520, 			// 95em
+        "tablet": 1280, 			// 80em
+        "tablet-portrait": 1000, 	// 62.5em
+        "mobile": 720, 				// 45em
+        "mobile-portrait": 440 		// 27.5em
     };
 
     function addRemoveClasses(attribute, addOrRemove, el) {
-        var attribs = el.getAttribute(attribute);
+        var attribs = el.getAttribute('class-' + attribute);
         if (attribs) {
             attribs.split(" ").forEach(function(element) {
                 if (addOrRemove == "add") {
-                    el.classList.add(element);
+                    if (!element.indexOf("/")) {
+                        el.classList.remove(element.slice(1));
+                    } else {
+                        el.classList.add(element);
+                    }
                 } else {
-                    el.classList.remove(element);
+                    if (!~element.indexOf("/")) {
+                        el.classList.remove(element);
+                    }
                 }
             });
         }
@@ -24,10 +30,23 @@
     function containerFunc(device, el) {
         var wW = window.innerWidth;
         if (wW < dims[device]) {
-            addRemoveClasses('class-' + device, 'add', el);
-            addRemoveClasses('class-' + device + '-remove', 'remove', el);
+            addRemoveClasses(device, 'add', el);
         } else {
-            addRemoveClasses('class-' + device, 'remove', el);
+            addRemoveClasses(device, 'remove', el);
+        }
+    }
+
+    function initialClasses(el) {
+        var ci = "class-initial";
+        if (!el.getAttribute(ci)) {
+            el.setAttribute(ci, el.className);
+        } else {
+            el.getAttribute(ci).split(" ").forEach(function(element) {
+                el.classList.add(element);
+            });
+        }
+        if (!el.getAttribute(ci).length) {
+            el.removeAttribute(ci);
         }
     }
 
@@ -43,10 +62,15 @@
         var classers = document.querySelectorAll('body [class]');
         var i = classers.length;
         while (i--) {
+
+            initialClasses(classers[i]);
+
             var k = Object.keys(dims);
-            var j = k.length;
-            while (j--) {
+            var j = 0;
+            var _j = k.length;
+            while (j < _j) {
                 containerFunc(k[j], classers[i]);
+                j++;
             }
         }
     }
