caba8c89cf23403b10137fb2554c6d8bcc0f9306
paulbrowne <Paulmatthewbrowne@gmail.com>
2018-06-09 15:40:37 +0300

    Update class-device-desktop-first.js

--- class-device-desktop-first.js
+++ class-device-desktop-first.js
@@ -1,11 +1,11 @@
 (function() {
 
     var dims = {
-        "laptop": 1520, 			// 95em
-        "tablet": 1280, 			// 80em
-        "tablet-portrait": 1000, 	// 62.5em
-        "mobile": 720, 				// 45em
-        "mobile-portrait": 440 		// 27.5em
+        "laptop": 1560,             // 97.5em
+        "tablet": 1280,             // 80em
+        "tablet-portrait": 1000,    // 62.5em
+        "mobile": 720,              // 45em
+        "mobile-portrait": 440      // 27.5em
     };
 
     function addRemoveClasses(attribute, el, addOrRemove) {
